1421e85064c19c6bb061db4210f10a4efad9e2f3
AshwinRajvanshi <ashwinrajvanshi@gmail.com>
2017-07-13 10:20:21 +0530

    Add files via upload

---
+++ Gaussian/Gaussian.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy
+img=cv2.imread('bookpage.jpg')
+retval,threshold1=cv2.threshold(img,12,255,cv2.THRESH_BINARY)
+grayscaled=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+retval2,threshold2=cv2.threshold(grayscaled,12,255,cv2.THRESH_BINARY)
+gaus=cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
+
+cv2.imshow('real',img)
+cv2.imshow('thresh',threshold1)
+cv2.imshow('Gray2',threshold2)
+cv2.imshow('gaus',gaus)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
